11806220ecaae6acdd17ec1eca1c525003776281
youngmu <tyg04308@naver.com>
2022-07-11 16:05:56 +0900

    [ADD] #11 - hilt setting

---
+++ presentation/src/main/java/com/woowahan/repositorysearch/di/module/ServiceModule.kt
@@ -0,0 +1,10 @@
+package com.woowahan.repositorysearch.di.module
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+}
