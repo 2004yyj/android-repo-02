bae388fc9e31920987e2954bf031a6a44deea606
youngmu <tyg04308@naver.com>
2022-07-11 21:23:37 +0900

    [ADD] #1 -Login Activity

---
+++ presentation/src/main/java/com/woowahan/repositorysearch/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.woowahan.repositorysearch.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.woowahan.repositorysearch.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
