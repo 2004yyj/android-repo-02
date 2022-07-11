bfbee8acf8c8062b1b508a72b50581aa4ed02912
양윤재 <diddbswo04@dgsw.hs.kr>
2022-07-11 21:46:25 +0900

    [ADD] ViewPager-TabLayout Connection to MainActivity

--- presentation/src/main/java/com/woowahan/repositorysearch/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/ui/main/MainActivity.kt
@@ -5,9 +5,15 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import coil.load
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.woowahan.repositorysearch.R
 import com.woowahan.repositorysearch.databinding.ActivityMainBinding
+import com.woowahan.repositorysearch.ui.adapter.ViewPagerAdapter
+import com.woowahan.repositorysearch.ui.main.fragment.IssueFragment
+import com.woowahan.repositorysearch.ui.main.fragment.NotificationFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,12 +23,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var ivUserIcon: ImageView
     private lateinit var userIconActionView: View
     private lateinit var searchMenu: MenuItem
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         init()
+        initTabLayout()
         initListener()
     }
 
@@ -30,11 +38,21 @@ class MainActivity : AppCompatActivity() {
         userIconActionView = toolbar.menu.findItem(R.id.user_icon).actionView
         searchMenu = toolbar.menu.findItem(R.id.search)
         ivUserIcon = userIconActionView.findViewById(R.id.iv_user_icon)
+        viewPagerAdapter = ViewPagerAdapter(
+            this@MainActivity,
+            listOf(IssueFragment(), NotificationFragment())
+        )
 
         //Todo: 추후 서버 연동 시 LiveData 에서 이미지 데이터 가져오기
         ivUserIcon.load("https://avatars.githubusercontent.com/u/18213322?v=4")
     }
 
+    private fun initTabLayout() = with(binding) {
+        viewPager.adapter = viewPagerAdapter
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = tabText[position]
+        }.attach()
+    }
 
     private fun initListener() {
         userIconActionView.setOnClickListener {
