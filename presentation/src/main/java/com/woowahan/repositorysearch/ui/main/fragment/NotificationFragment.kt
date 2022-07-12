429f648980a5f0a7315cd517b30468790e3d7539
Youngmu <tyg04308@naver.com>
2022-07-12 22:14:31 +0900

    [ADD] #5 - RecyclerView in Notification fragment

--- presentation/src/main/java/com/woowahan/repositorysearch/ui/main/fragment/NotificationFragment.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/ui/main/fragment/NotificationFragment.kt
@@ -1,10 +1,10 @@
 package com.woowahan.repositorysearch.ui.main.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.woowahan.repositorysearch.databinding.FragmentNotificationBinding
 
 class NotificationFragment : Fragment() {
@@ -17,5 +17,10 @@ class NotificationFragment : Fragment() {
     ): View {
         binding = FragmentNotificationBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
