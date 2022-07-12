5a640142d07bf6a85d683c4182ddfc68c69666ab
양윤재 <diddbswo04@dgsw.hs.kr>
2022-07-12 12:33:35 +0900

    [ADD] getIntent Companion Object Function Add

--- presentation/src/main/java/com/woowahan/repositorysearch/ui/result/ResultActivity.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/ui/result/ResultActivity.kt
@@ -1,9 +1,18 @@
 package com.woowahan.repositorysearch.ui.result
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import com.woowahan.repositorysearch.R
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
+import androidx.fragment.app.FragmentContainerView
 import com.woowahan.repositorysearch.databinding.ActivityResultBinding
+import com.woowahan.repositorysearch.ui.result.fragment.ProfileFragment
+import com.woowahan.repositorysearch.ui.result.fragment.SearchFragment
+import com.woowahan.repositorysearch.util.replace
+import java.io.Serializable
 
 class ResultActivity : AppCompatActivity() {
 
@@ -12,5 +21,31 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+    }
+
+    companion object {
+        const val LOGIN_VALUE = "LOGIN"
+        private const val START_FRAGMENT_NAME = "START_FRAGMENT"
+
+        // 검색 첫 화면
+        fun getIntent(context: Context, pageName: PageName.Search): Intent {
+            return Intent(context, ResultActivity::class.java).apply {
+                putExtra(START_FRAGMENT_NAME, pageName)
+            }
+        }
+
+        // 프로필 첫 화면
+        fun getIntent(context: Context, pageName: PageName.Profile, login: String): Intent {
+            return Intent(context, ResultActivity::class.java).apply {
+                putExtra(START_FRAGMENT_NAME, pageName)
+                putExtra(LOGIN_VALUE, login)
+            }
+        }
+    }
+
+    sealed class PageName: Serializable {
+        object Profile : PageName()
+        object Search: PageName()
     }
 }
