19f6f36a2b68e070a463a0d98f14fe69a80a8dcb
youngmu <tyg04308@naver.com>
2022-07-12 16:15:26 +0900

    [FEAT] #1 - Github login

--- presentation/src/main/java/com/woowahan/repositorysearch/ui/login/LoginActivity.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/ui/login/LoginActivity.kt
@@ -4,15 +4,19 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.viewModelScope
+import com.woowahan.domain.model.GitToken
 import com.woowahan.repositorysearch.BuildConfig
 import com.woowahan.repositorysearch.R
 import com.woowahan.repositorysearch.databinding.ActivityLoginBinding
+import com.woowahan.repositorysearch.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
@@ -41,8 +45,18 @@ class LoginActivity : AppCompatActivity() {
         super.onNewIntent(intent)
         intent?.data?.getQueryParameter("code")?.let { code ->
             viewModel.viewModelScope.launch {
-                val token = viewModel.getAccessToken(code)
-                print(token)
+                try {
+                    val token = viewModel.getAccessToken(code)
+                    GitToken.token = token.token
+                    GitToken.scope = token.scope
+                    GitToken.type = token.type
+
+                    val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
+                    mainIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    startActivity(mainIntent)
+                } catch (e: Exception) {
+                    Toast.makeText(this@LoginActivity, e.message, Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
