647dc67fc956781326432364fc1e735822f7aa88
youngmu <tyg04308@naver.com>
2022-07-13 16:01:52 +0900

    [FEAT] #5 - NotificationService

--- data/src/main/java/com/woowahan/data/notification/NotificationService.kt
+++ data/src/main/java/com/woowahan/data/notification/NotificationService.kt
@@ -1,4 +1,12 @@
 package com.woowahan.data.notification
 
+import com.woowahan.data.entity.NotificationData
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
 interface NotificationService {
+    @GET("/notifications")
+    @Headers("Accept: application/vnd.github+json")
+    suspend fun getNotifications(): Response<NotificationData>
 }
