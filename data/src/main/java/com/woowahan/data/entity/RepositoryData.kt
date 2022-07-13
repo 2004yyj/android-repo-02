5df96cdadb692576a0472c352240776131c73955
youngmu <tyg04308@naver.com>
2022-07-13 16:09:39 +0900

    [REFACTOR] #5 - move commentsUrl from NotificationData to RepositoryData

--- data/src/main/java/com/woowahan/data/entity/RepositoryData.kt
+++ data/src/main/java/com/woowahan/data/entity/RepositoryData.kt
@@ -3,5 +3,6 @@ package com.woowahan.data.entity
 data class RepositoryData(
     val id: Int,
     val fullName: String,
-    val owner: OwnerData
+    val owner: OwnerData,
+    val commentsUrl: String,
 )
