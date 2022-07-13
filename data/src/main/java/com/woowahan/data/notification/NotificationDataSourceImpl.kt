e67e79dc2c284112435ae0d19165d8f3280d32e8
youngmu <tyg04308@naver.com>
2022-07-13 16:03:11 +0900

    [FEAT] #5 - NotificationDataSourceImpl

--- data/src/main/java/com/woowahan/data/notification/NotificationDataSourceImpl.kt
+++ data/src/main/java/com/woowahan/data/notification/NotificationDataSourceImpl.kt
@@ -1,7 +1,9 @@
 package com.woowahan.data.notification
 
 import com.woowahan.data.base.BaseDataSource
+import com.woowahan.data.entity.NotificationData
 
 class NotificationDataSourceImpl(override val service: NotificationService) :
     BaseDataSource<NotificationService>() {
+    suspend fun getNotifications(): NotificationData = getData(service.getNotifications())
 }
