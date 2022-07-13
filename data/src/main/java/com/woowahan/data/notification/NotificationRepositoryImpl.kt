ad463af894181fbde10d1a37c54c77f2eb77f818
youngmu <tyg04308@naver.com>
2022-07-13 16:07:47 +0900

    [FEAT] #5 - NotificationRepositoryImpl

--- data/src/main/java/com/woowahan/data/notification/NotificationRepositoryImpl.kt
+++ data/src/main/java/com/woowahan/data/notification/NotificationRepositoryImpl.kt
@@ -1,10 +1,23 @@
 package com.woowahan.data.notification
 
 import com.woowahan.data.auth.AuthRepositoryImpl
+import com.woowahan.domain.model.Notification
 import com.woowahan.domain.repository.NotificationRepository
 import javax.inject.Inject
 
 class NotificationRepositoryImpl @Inject constructor(
     private val notificationDataSourceImpl: NotificationDataSourceImpl
 ) : NotificationRepository {
+    override suspend fun getNotifications(): Notification {
+        val notificationData = notificationDataSourceImpl.getNotifications()
+        val commentCnt = notificationData.commentsUrl.split("/").last().replace("comments", "")
+        return Notification(
+            notificationData.id.toInt(),
+            notificationData.reason,
+            notificationData.updatedAt,
+            notificationData.repository.fullName,
+            commentCnt.toInt(),
+            notificationData.repository.owner.avatarUrl
+        )
+    }
 }
