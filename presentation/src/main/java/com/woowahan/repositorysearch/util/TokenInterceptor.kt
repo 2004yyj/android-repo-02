7294956c98dc67450620a62248ca3c6b232e666e
youngmu <tyg04308@naver.com>
2022-07-13 23:31:04 +0900

    [FIX] #5 - tokenInterceptor 코드 수정

--- presentation/src/main/java/com/woowahan/repositorysearch/util/TokenInterceptor.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/util/TokenInterceptor.kt
@@ -1,15 +1,18 @@
 package com.woowahan.repositorysearch.util
 
+import com.woowahan.domain.model.GitToken
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class TokenInterceptor: Interceptor {
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-        request
-            .newBuilder()
-            .addHeader("client_id", "21e566da1e2ebde399b4")
-            .build()
-        return chain.proceed(request)
+class TokenInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response  = with(chain){
+        val newRequest = request().newBuilder()
+        if (GitToken.token.isNotEmpty()) {
+            newRequest.addHeader("X-OAuth-Scopes", "notification")
+            newRequest.addHeader("Authorization", GitToken.token)
+        }
+        val build = newRequest.build()
+
+        return proceed(build)
     }
 }
