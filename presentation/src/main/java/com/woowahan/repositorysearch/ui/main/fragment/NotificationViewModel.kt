517128621a52b51b8dbb64dc855641de3c649cb9
youngmu <tyg04308@naver.com>
2022-07-15 01:18:37 +0900

    [FEAT] #59 - Remove all notifications

--- presentation/src/main/java/com/woowahan/repositorysearch/ui/main/fragment/NotificationViewModel.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/ui/main/fragment/NotificationViewModel.kt
@@ -1,28 +1,25 @@
 package com.woowahan.repositorysearch.ui.main.fragment
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.woowahan.domain.model.GitToken
-import com.woowahan.domain.model.Notification
-import com.woowahan.domain.model.Result
-import com.woowahan.domain.model.User
+import com.woowahan.domain.model.*
 import com.woowahan.domain.notificationUseCase.GetNotificationsUseCase
-import com.woowahan.repositorysearch.BuildConfig
+import com.woowahan.domain.notificationUseCase.MarkNotificationAsReadUseCase
 import com.woowahan.repositorysearch.di.module.RetrofitModule
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class NotificationViewModel @Inject constructor(
-    @RetrofitModule.typeApi private val getNotificationsUseCase: GetNotificationsUseCase
+    @RetrofitModule.typeApi private val getNotificationsUseCase: GetNotificationsUseCase,
+    @RetrofitModule.typeApi private val markNotificationAsReadUseCase: MarkNotificationAsReadUseCase
 ) : ViewModel() {
     private val _notifications = MutableSharedFlow<List<Notification>>()
     val notifications = _notifications.asSharedFlow()
@@ -30,6 +27,12 @@ class NotificationViewModel @Inject constructor(
     private val _isFailure = MutableSharedFlow<Throwable>()
     val isFailure = _isFailure.asSharedFlow()
 
+
+    private val _isMarkedSuccess = MutableSharedFlow<Message>()
+    val isMarkedSuccess = _isMarkedSuccess.asSharedFlow()
+
+    private val _isMarkedFail = MutableSharedFlow<Throwable>()
+    val isMarkedFail = _isMarkedFail.asSharedFlow()
 
     fun getNotifications() {
         val noti = getNotificationsUseCase.execute(1)
@@ -45,5 +48,28 @@ class NotificationViewModel @Inject constructor(
                 is Result.Loading -> {}
             }
         }.launchIn(viewModelScope)
+    }
+
+    fun markNotificationAsRead(threadId: String) {
+        markNotificationAsReadUseCase.execute(getCurrentTimeString(), threadId).onEach { result ->
+            when (result) {
+                is Result.Success -> {
+                    _isMarkedSuccess.emit(result.data)
+                }
+                is Result.Failure -> {
+                    _isMarkedFail.emit(result.throwable)
+                }
+                is Result.Loading -> {}
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun getCurrentTimeString(): String {
+        val tz = TimeZone.getTimeZone("UTC")
+        val df: DateFormat =
+            SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'")
+
+        df.timeZone = tz
+        return df.format(Date())
     }
 }
