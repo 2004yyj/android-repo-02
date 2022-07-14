cfc13d2ade200d4d5ad28cb88bb797e41ff619ce
youngmu <tyg04308@naver.com>
2022-07-14 11:00:21 +0900

    [FEAT] #5 - Notification data와 RecyclerView 연동

--- presentation/src/main/java/com/woowahan/repositorysearch/ui/main/MainViewModel.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/ui/main/MainViewModel.kt
@@ -4,24 +4,30 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.woowahan.domain.model.GitToken
+import com.woowahan.domain.model.Notification
 import com.woowahan.domain.notificationUseCase.GetNotificationsUseCase
-import com.woowahan.repositorysearch.BuildConfig
 import com.woowahan.repositorysearch.di.module.RetrofitModule
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     @RetrofitModule.typeApi private val getNotificationsUseCase: GetNotificationsUseCase
 ) : ViewModel() {
+    private val _liveNotifications = MutableLiveData<List<Notification>>()
+    val liveNotifications: LiveData<List<Notification>>
+        get() = _liveNotifications
 
     fun getNotifications() {
         viewModelScope.launch(Dispatchers.IO) {
-            val test = getNotificationsUseCase.execute(1)
-            print(test)
+            try {
+                _liveNotifications.postValue(getNotificationsUseCase.execute(1))
+            } catch (e: Exception) {
+
+            }
         }
     }
 }
