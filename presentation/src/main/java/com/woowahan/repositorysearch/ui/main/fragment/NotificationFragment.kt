2c2a20c4ae46277180e46dfc26a05efc0f0913bb
youngmu <tyg04308@naver.com>
2022-07-15 00:53:47 +0900

    [FEAT] #59 - Notification swipe animation

--- presentation/src/main/java/com/woowahan/repositorysearch/ui/main/fragment/NotificationFragment.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/ui/main/fragment/NotificationFragment.kt
@@ -1,5 +1,8 @@
 package com.woowahan.repositorysearch.ui.main.fragment
 
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,13 +11,15 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
-import coil.load
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.woowahan.repositorysearch.R
 import com.woowahan.repositorysearch.databinding.FragmentNotificationBinding
 import com.woowahan.repositorysearch.ui.adapter.NotificationAdapter
 import com.woowahan.repositorysearch.ui.main.DividerItemDecoration
 import com.woowahan.repositorysearch.util.Dp2Px
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class NotificationFragment : Fragment() {
@@ -54,5 +59,64 @@ class NotificationFragment : Fragment() {
                 }
             }
         }
+
+        ItemTouchHelper(recyclerViewTouchCallback).attachToRecyclerView(binding.rvNotification)
     }
+
+    private val recyclerViewTouchCallback =
+        object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                print("TODO, Will be mark")
+            }
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isActiveNow: Boolean
+            ) {
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isActiveNow)
+
+                if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+                    val icon =
+                        ContextCompat.getDrawable(requireContext(), R.drawable.ic_check) ?: return
+
+                    val view = viewHolder.itemView
+                    val paint = Paint()
+                    paint.color = ContextCompat.getColor(requireContext(), R.color.primary)
+
+                    val icPadding = Dp2Px.convert(requireContext(), 48F)
+                    val icLeft = (view.right - icPadding - icon.intrinsicWidth).toInt()
+                    val icTop = view.top + ((view.bottom - view.top - icon.intrinsicHeight) / 2)
+                    val icRight = (view.right - icPadding).toInt()
+                    val icBottom = icTop + icon.intrinsicHeight
+
+                    if (dX == 0F) {
+                        icon.bounds = Rect(0, 0, 0, 0)
+                    } else {
+                        c.drawRect(
+                            view.right.toFloat() + dX,
+                            view.top.toFloat(),
+                            view.right.toFloat(),
+                            view.bottom.toFloat(),
+                            paint
+                        )
+
+                        icon.bounds = Rect(icLeft, icTop, icRight, icBottom)
+                        icon.draw(c)
+                    }
+                }
+            }
+        }
 }
