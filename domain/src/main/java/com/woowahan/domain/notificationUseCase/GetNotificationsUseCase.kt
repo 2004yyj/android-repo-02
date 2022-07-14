64ed5ee8730198127e1a9687e292cc6da2e09457
youngmu <tyg04308@naver.com>
2022-07-14 13:55:22 +0900

    [REFACTOR] #52 - change coroutine to coroutine flow in notification

--- domain/src/main/java/com/woowahan/domain/notificationUseCase/GetNotificationsUseCase.kt
+++ domain/src/main/java/com/woowahan/domain/notificationUseCase/GetNotificationsUseCase.kt
@@ -1,7 +1,19 @@
 package com.woowahan.domain.notificationUseCase
 
+import com.woowahan.domain.model.Result
 import com.woowahan.domain.repository.NotificationRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class GetNotificationsUseCase(private val repository: NotificationRepository) {
-    suspend fun execute(page: Int) = repository.getNotifications(page)
+    fun execute(page: Int) = flow {
+        emit(Result.Loading)
+        try {
+            val notifications = repository.getNotifications(page)
+            emit(Result.Success(notifications))
+        } catch (e: Throwable) {
+            emit(Result.Failure(e))
+        }
+    }.flowOn(Dispatchers.IO)
 }
