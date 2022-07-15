adb2a68a7c3795482d46c932057e28f345520bd8
양윤재 <diddbswo04@dgsw.hs.kr>
2022-07-15 10:34:38 +0900

    [ADD] 스피너 초기화 코드

--- presentation/src/main/java/com/woowahan/repositorysearch/ui/main/fragment/IssueFragment.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/ui/main/fragment/IssueFragment.kt
@@ -1,15 +1,22 @@
 package com.woowahan.repositorysearch.ui.main.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import com.woowahan.repositorysearch.databinding.FragmentIssueBinding
+import com.woowahan.repositorysearch.ui.adapter.FilterAdapter
 
 class IssueFragment : Fragment() {
 
     private lateinit var binding: FragmentIssueBinding
+    private val filterItem = listOf("Opened", "Closed", "All")
+    private val filterAdapter: FilterAdapter by lazy {
+        FilterAdapter(requireContext(), filterItem)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +24,29 @@ class IssueFragment : Fragment() {
     ): View {
         binding = FragmentIssueBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initSpinner()
+    }
+
+    private fun initSpinner() {
+        binding.spiFilter.adapter = filterAdapter
+        binding.spiFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val query = filterItem[position]
+                // 이슈 검색하기
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
     }
 }
