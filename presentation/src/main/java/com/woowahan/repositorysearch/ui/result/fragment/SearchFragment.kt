fe39fbcf7acbd901b4994ffd26d3579068409b57
양윤재 <diddbswo04@dgsw.hs.kr>
2022-07-18 21:14:58 +0900

    [ADD] ProgressBar

--- presentation/src/main/java/com/woowahan/repositorysearch/ui/result/fragment/SearchFragment.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/ui/result/fragment/SearchFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -71,14 +72,8 @@ class SearchFragment : Fragment() {
         rvSearch.adapter = searchAdapter
 
         searchAdapter.addLoadStateListener {
-            when (it.append) {
-                is LoadState.Loading -> {
-                    loading.show(parentFragmentManager, "Loading")
-                }
-                is LoadState.NotLoading -> {
-                    loading.dismiss()
-                }
-            }
+            pbReload.isVisible =
+                it.source.refresh is LoadState.Loading
         }
     }
 
