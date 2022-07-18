bd195a9b7e99ce12ec65a19f092633b27acf1b98
youngmu <tyg04308@naver.com>
2022-07-18 18:14:13 +0900

    [ADD] #6 - Get star di setting

--- presentation/src/main/java/com/woowahan/repositorysearch/di/module/RepositoryModule.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/di/module/RepositoryModule.kt
@@ -7,13 +7,13 @@ import com.woowahan.data.issue.IssueDataSourceImpl
 import com.woowahan.data.issue.IssueRepositoryImpl
 import com.woowahan.data.notification.NotificationDataSourceImpl
 import com.woowahan.data.notification.NotificationRepositoryImpl
+import com.woowahan.data.search.SearchDataSourceImpl
+import com.woowahan.data.search.SearchRepositoryImpl
 import com.woowahan.data.user.UserDataSourceImpl
 import com.woowahan.data.user.UserRepositoryImpl
 import com.woowahan.domain.model.Issue
-import com.woowahan.domain.repository.AuthRepository
-import com.woowahan.domain.repository.IssueRepository
-import com.woowahan.domain.repository.NotificationRepository
-import com.woowahan.domain.repository.UserRepository
+import com.woowahan.domain.model.Repository
+import com.woowahan.domain.repository.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -46,4 +46,13 @@ object RepositoryModule {
     @RetrofitModule.typeApi
     fun provideIssueRepository(@RetrofitModule.typeApi issueDataSourceImpl: IssueDataSourceImpl): IssueRepository<PagingData<Issue>> =
         IssueRepositoryImpl(issueDataSourceImpl)
+
+    @Singleton
+    @Provides
+    @RetrofitModule.typeApi
+    fun provideSearchRepository(
+        @RetrofitModule.typeApi searchDataSourceImpl: SearchDataSourceImpl,
+        @RetrofitModule.typeApi userDataSourceImpl: UserDataSourceImpl
+    ): SearchRepository<PagingData<Repository>> =
+        SearchRepositoryImpl(searchDataSourceImpl, userDataSourceImpl)
 }
