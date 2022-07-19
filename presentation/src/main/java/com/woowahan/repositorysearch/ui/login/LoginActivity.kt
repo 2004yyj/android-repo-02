2463fbd1e5e322aecba98f4d6c376edd9579b765
youngmu <tyg04308@naver.com>
2022-07-19 16:09:51 +0900

    [FEAT] #98 - Login button 더블클릭 방지

--- presentation/src/main/java/com/woowahan/repositorysearch/ui/login/LoginActivity.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/ui/login/LoginActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
@@ -24,6 +26,7 @@ import java.lang.Exception
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private val viewModel by viewModels<LoginViewModel>()
+    private var lastClickTime = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,17 +35,20 @@ class LoginActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
 
         binding.btnLogin.setOnClickListener {
-            val loginUrl = Uri.Builder().scheme("https").authority("github.com")
-                .appendPath("login")
-                .appendPath("oauth")
-                .appendPath("authorize")
-                .appendQueryParameter("client_id", BuildConfig.CLIENT_ID)
-                .appendQueryParameter("scope", "repo,notifications,user")
-                .build()
-
-            CustomTabsIntent.Builder().build().also {
-                it.launchUrl(this, loginUrl)
+            if (SystemClock.elapsedRealtime() - lastClickTime > 300) {
+                val loginUrl = Uri.Builder().scheme("https").authority("github.com")
+                    .appendPath("login")
+                    .appendPath("oauth")
+                    .appendPath("authorize")
+                    .appendQueryParameter("client_id", BuildConfig.CLIENT_ID)
+                    .appendQueryParameter("scope", "repo,notifications,user")
+                    .build()
+                
+                CustomTabsIntent.Builder().build().also {
+                    it.launchUrl(this, loginUrl)
+                }
             }
+            lastClickTime = SystemClock.elapsedRealtime()
         }
 
         lifecycleScope.launchWhenStarted {
