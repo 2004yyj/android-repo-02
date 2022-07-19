96be4967933d2ec6e883434fcbe0e3b0e34e09fb
youngmu <tyg04308@naver.com>
2022-07-19 14:28:21 +0900

    [FEAT] #41 - Loading

--- presentation/src/main/java/com/woowahan/repositorysearch/ui/main/fragment/NotificationFragment.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/ui/main/fragment/NotificationFragment.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.ItemTouchHelper
 import com.woowahan.repositorysearch.R
 import com.woowahan.repositorysearch.databinding.FragmentNotificationBinding
