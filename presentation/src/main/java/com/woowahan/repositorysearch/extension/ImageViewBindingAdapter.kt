7b11eb3c8303222475db7939b8e2d8d26133d17d
양윤재 <diddbswo04@dgsw.hs.kr>
2022-07-21 16:55:14 +0900

    [FIX] issue state에 따라 image 변하지 않는 오류

--- presentation/src/main/java/com/woowahan/repositorysearch/extension/ImageViewBindingAdapter.kt
+++ presentation/src/main/java/com/woowahan/repositorysearch/extension/ImageViewBindingAdapter.kt
@@ -4,13 +4,17 @@ import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
+import com.woowahan.repositorysearch.R
 
 @BindingAdapter("android:loadImage")
 fun ImageView.setImage(data: String?) {
     load(data)
 }
 
-@BindingAdapter("android:loadImage")
-fun ImageView.setImage(data: Drawable) {
-    load(data)
+@BindingAdapter("android:issueCheck")
+fun ImageView.issueCheck(state: String) {
+    if (state == "open")
+        load(R.drawable.ic_issue_open)
+    else
+        load(R.drawable.ic_issue_closed)
 }
